e8b69a87bd12285c7eb389e5bace78e4fe9815b0
Rohan Kanigere Umesh <kurohan147@gmail.com>
2026-09-08 20:14:37 -0400

    Front end code

--- app/main.py
+++ app/main.py
@@ -3,10 +3,19 @@ from app.core.config import settings
 from app.api.health import router
 from app.api.users import router as users_router
 from app.api.orders import router as orders_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
